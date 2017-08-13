78c5b5d9679fcd8ec27a0831318ed94721b9bf67
“Surfboy678 <janusz.brodacki@gmail.com>
2017-08-13 18:23:20 +0200

    test

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,4 +1,31 @@
 package com.kodilla.testing;
 
-public class TestingMain {
-}
+import calculator.Calculator;
+
+
+    public class TestingMain {
+        public static void main(String[] args) {
+            Calculator calculator = new Calculator();
+
+            int result1 = calculator.addAToB(5, 5);
+            int result2 = calculator.substractAFromB(10, 8);
+            if (result1 == 10) {
+                if (result2 == 2) ;
+                {
+                    System.out.println("test OK");
+
+                    if(result1 != 10){
+                        if(result2 != 2)
+                        {
+                            System.out.println("Test error");
+                        }
+
+                    }
+
+
+                }
+            }
+        }
+    }
+
+
