45946fd9ab8e58b68b761837728db2b17d344dbc
Surfboy678 <Surfboy678@users.noreply.github.com>
2017-10-19 17:45:07 +0200

    Commit

---
+++ kodilla-hibernate2/src/main/java/com/kodilla/hibernate2/manytomany/dao/CompanyDao.java
@@ -0,0 +1,20 @@
+package com.kodilla.hibernate2.manytomany.dao;
+
+import com.kodilla.hibernate2.manytomany.Company;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import javax.transaction.Transactional;
+import java.util.List;
+
+
+@Transactional
+@Repository
+public interface CompanyDao extends CrudRepository<Company, Integer> {
+
+    @Query
+    List<Company> retrieveCompanyWhoseNameBegins(@Param("STRING") String string);
+
+
+}
