6f5e75171b7bacc07a1c51925f91b5959d5244c0
Surfboy678 <Surfboy678@users.noreply.github.com>
2017-12-25 23:43:52 +0100

    Commit

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/paterns/challenges/challenges2/Application.java
@@ -0,0 +1,13 @@
+package com.kodilla.good.paterns.challenges.challenges2;
+
+
+import java.util.ArrayList;
+
+public class Application {
+    public static void main (String args[]){
+
+
+
+
+    }
+}
